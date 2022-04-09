5e4442c1d179771cbf513c3270accca2d4a56c1c
Vinayak Mani (202132) <89988949+VinayakMani@users.noreply.github.com>
2022-04-09 11:11:07 +0530

    Add files via upload

---
+++ L10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// int i =10;
+// // while loop
+// while(i<=50){
+//     cout<<i<<endl;
+//     i++;
+// }
+
+// for(int i = 0;i<=10;i++){
+// cout<<i<<endl;
+// }
+
+int i=1;
+do{
+cout<<i<<endl;
+i++;
+
+}while(i<=41);
+return 0;
+}
